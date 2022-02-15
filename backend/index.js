5ba028d2c37c83d8a8995ca0e619a0aa0f0cceb8
valtteriNummi <valtteri@hiisihomes.fi>
2022-02-15 14:26:24 +0200

    fixed some errors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 
 export const app = express()
-export const port = () => 5000
+export const port = () => {return 5000}
 
 app.get('/', (req, res) => {
     res.send({val: 'Hello World'})
 })
+
+app.listen(port())
+console.log(`App running at http://localhost:${port()}`)
 
 export default app;
